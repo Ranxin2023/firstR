14831f019aaddc0efa7fc77785d76fac83999f7c
Ranxin <rstli@ucdavis.edu>
2024-01-15 10:39:25 -0800

    first commit

---
+++ firstR.R
@@ -0,0 +1,33 @@
+print("hello world")
+print(5+5)
+for(x in 1:10){
+  print(x)
+}
+name <- "John Smith"
+paste("hello! ", name)
+x <- y <- z <- 5
+x
+y
+z
+
+nums <- list(1, 2, 6, 4, 5)
+for(num in nums){
+  print(num)
+}
+
+helloworld <- function(){
+  print("helloworld")
+}
+
+helloworld()
+helloname <-function(name){
+  paste("hello", name)
+}
+helloname(name)
+factorial <- function(n){
+  if(n==0){
+    return(1)
+  }
+  return(n*factorial(n-1))
+}
+factorial(8)
